108174ac974942d2e07103f8de55c6167e4f7642
pat-rapidai <sheehan@rapid.ai>
2020-05-06 01:05:58 -0700

    (feat) Add some console logging to inform the user

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var mllp = require('mllp-node');
 
+var host = '127.0.0.1'
+var port = 1234
 var server = new mllp.MLLPServer('127.0.0.1', 1234);
+
+console.log('Listening for HL7 messages on:', host, ':', port, '...')
+console.log('\nPress ^C to exit\n')
 
 // Subscribe to inbound messages
 server.on('hl7', function (data) {
